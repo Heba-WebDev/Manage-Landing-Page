00b7c151a5f4f8087f42be4d4defd511282c8665
Heba-Webdev23 <jtvonline@yahoo.com>
2022-11-08 04:40:13 +0300

    added mobile navbar

--- index.js
+++ index.js
@@ -1,96 +1,131 @@
 
-let isMobileNavOpen = false;
+// the state the shows if the mobile menu is showen or not
+let isNavMenuOpen = false;
+
+// the mobile menu button
+let mobileMenuBtn = document.querySelector(".navbar__mobile");
+
+// the hamburger/close icon
 let mobileNavIcon = document.querySelector(".navbar__mobile__icon");
+// the hamburger/close img src 
+let navOpenIconSrc = "/images/icon-hamburger.svg";
+let NavCloseIconSrc = "/images/icon-close.svg";
 
-mobileNavIcon.addEventListener('click', openMobileNav);
 
-function openMobileNav() {
-    isMobileNavOpen = !isMobileNavOpen;
-
-    if(isMobileNavOpen) {
-        mobileNavIcon.src = "/images/icon-close.svg";
-    } else {
-        mobileNavIcon.src = "/images/icon-hamburger.svg";
-    }
-}
-
-let testimonal1 = document.getElementById("testimonails--1");
-let testimonal2 = document.getElementById("testimonails--2");
-let testimonal3 = document.getElementById("testimonails--3");
-let testimonal4 = document.getElementById("testimonails--4");
+// the mobile menu list items
+ let mobileNavbar = document.querySelector(".navbar__menu ")
 
 
 
-let sliderButton1 = document.querySelector("#testimonailsbtn--1");
-let sliderButton2 = document.querySelector("#testimonailsbtn--2");
-let sliderButton3 = document.querySelector("#testimonailsbtn--3");
-let sliderButton4 = document.querySelector("#testimonailsbtn--4");
 
-sliderButton1.addEventListener('click', getFirstTestimonial)
+ mobileMenuBtn.addEventListener("click", toggleMobileNavbar)
+ 
+ function toggleMobileNavbar() {
 
-
-function getFirstTestimonial() {
-    testimonal1.style.display = 'grid'
-    sliderButton1.classList.add('selected');
-
-    testimonal2.style.display = 'none'
-    testimonal3.style.display = 'none'
-    testimonal4.style.display = 'none'
-    sliderButton2.classList.remove('selected');
-    sliderButton3.classList.remove('selected');
-    sliderButton4.classList.remove('selected');
-}
+   if(!isNavMenuOpen) {
+      mobileNavbar. style.display = "block"
+      isNavMenuOpen = true;
+      mobileNavIcon.src = NavCloseIconSrc;
+      document.body.style.overflow = "hidden";
+   } else {
+      mobileNavbar. style.display = "none" 
+      isNavMenuOpen = false;
+      mobileNavIcon.src = navOpenIconSrc;
+      document.body.style.overflow = "";
+   }
+};
 
 
 
-sliderButton2.addEventListener('click', getSecondTestimonial)
+// mobileNavIcon.addEventListener('click', openMobileNavMenu);
 
+// function openMobileNavMenu() {
+   
 
-function getSecondTestimonial() {
-    testimonal2.style.display = 'grid'
-    sliderButton2.classList.add('selected');
+//     if(isMobileNavOpen) {
+//         mobileNavIcon.src = "/images/icon-close.svg";
+//     } else {
+//         mobileNavIcon.src = "/images/icon-hamburger.svg";
+//     }
+// }
 
-    testimonal1.style.display = 'none'
-    testimonal3.style.display = 'none'
-    testimonal4.style.display = 'none'
-    sliderButton1.classList.remove('selected');
-    sliderButton3.classList.remove('selected');
-    sliderButton4.classList.remove('selected');
-}
-
-
-
-sliderButton3.addEventListener('click', getThirdTestimonial)
-
-
-function getThirdTestimonial() {
-    testimonal3.style.display = 'grid'
-    sliderButton3.classList.add('selected');
-
-    testimonal1.style.display = 'none'
-    testimonal2.style.display = 'none'
-    testimonal4.style.display = 'none'
-    sliderButton1.classList.remove('selected');
-    sliderButton2.classList.remove('selected');
-    sliderButton4.classList.remove('selected');
-}
+// let testimonal1 = document.getElementById("testimonails--1");
+// let testimonal2 = document.getElementById("testimonails--2");
+// let testimonal3 = document.getElementById("testimonails--3");
+// let testimonal4 = document.getElementById("testimonails--4");
 
 
 
-sliderButton4.addEventListener('click', getFourthTestimonial)
+// let sliderButton1 = document.querySelector("#testimonailsbtn--1");
+// let sliderButton2 = document.querySelector("#testimonailsbtn--2");
+// let sliderButton3 = document.querySelector("#testimonailsbtn--3");
+// let sliderButton4 = document.querySelector("#testimonailsbtn--4");
+
+// sliderButton1.addEventListener('click', getFirstTestimonial)
 
 
-function getFourthTestimonial() {
-    testimonal4.style.display = 'grid'
-    sliderButton4.classList.add('selected');
+// function getFirstTestimonial() {
+//     testimonal1.style.display = 'grid'
+//     sliderButton1.classList.add('selected');
 
-    testimonal1.style.display = 'none'
-    testimonal2.style.display = 'none'
-    testimonal3.style.display = 'none'
-    sliderButton1.classList.remove('selected');
-    sliderButton2.classList.remove('selected');
-    sliderButton3.classList.remove('selected');
-}
+//     testimonal2.style.display = 'none'
+//     testimonal3.style.display = 'none'
+//     testimonal4.style.display = 'none'
+//     sliderButton2.classList.remove('selected');
+//     sliderButton3.classList.remove('selected');
+//     sliderButton4.classList.remove('selected');
+// }
+
+
+
+// sliderButton2.addEventListener('click', getSecondTestimonial)
+
+
+// function getSecondTestimonial() {
+//     testimonal2.style.display = 'grid'
+//     sliderButton2.classList.add('selected');
+
+//     testimonal1.style.display = 'none'
+//     testimonal3.style.display = 'none'
+//     testimonal4.style.display = 'none'
+//     sliderButton1.classList.remove('selected');
+//     sliderButton3.classList.remove('selected');
+//     sliderButton4.classList.remove('selected');
+// }
+
+
+
+// sliderButton3.addEventListener('click', getThirdTestimonial)
+
+
+// function getThirdTestimonial() {
+//     testimonal3.style.display = 'grid'
+//     sliderButton3.classList.add('selected');
+
+//     testimonal1.style.display = 'none'
+//     testimonal2.style.display = 'none'
+//     testimonal4.style.display = 'none'
+//     sliderButton1.classList.remove('selected');
+//     sliderButton2.classList.remove('selected');
+//     sliderButton4.classList.remove('selected');
+// }
+
+
+
+// sliderButton4.addEventListener('click', getFourthTestimonial)
+
+
+// function getFourthTestimonial() {
+//     testimonal4.style.display = 'grid'
+//     sliderButton4.classList.add('selected');
+
+//     testimonal1.style.display = 'none'
+//     testimonal2.style.display = 'none'
+//     testimonal3.style.display = 'none'
+//     sliderButton1.classList.remove('selected');
+//     sliderButton2.classList.remove('selected');
+//     sliderButton3.classList.remove('selected');
+// }
 
 let emailRegx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
